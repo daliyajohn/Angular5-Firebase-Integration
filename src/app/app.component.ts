3f34d31e29dc8e9276bd9bb1e77dae446bea683e
daliyajohn <daliyajohn75@gmail.com>
2018-04-28 11:22:49 +0530

    List data implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import { map, catchError } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,13 +11,24 @@ import { Observable } from 'rxjs/Observable';
 export class AppComponent {
   title = 'app';
   items: any;
-  constructor(db: AngularFireDatabase) {
-    db
+
+  constructor(private db: AngularFireDatabase) {
+    this.db
       .list('/items')
       .snapshotChanges()
       .map(res => {
-        this.items = res;
+        console.log(res);
       });
-    console.log('falllmllnml', this.items);
+    // .snapshotChanges()
+    // .pipe(
+    //   map(res => {
+    //     this.items = res;
+    //     console.log('falllmllnml', this.items);
+    //   }),
+    //   catchError((error: any) => {
+    //     console.log(error.json());
+    //     return Observable.throw(error.json());
+    //   })
+    // );
   }
 }
